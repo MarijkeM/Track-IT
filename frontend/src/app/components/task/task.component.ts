122558e223a901b74307916a142797da2efeca0f
MarijkeM <marijkemeersman@gmail.com>
2017-12-30 23:18:55 +0100

    tasks tonen met task components

--- frontend/src/app/components/task/task.component.ts
+++ frontend/src/app/components/task/task.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Task} from '../../models/task'
+import {TaskService} from '../../services/taskService/task.service'
 
 @Component({
-  selector: 'app-task',
-  templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+    selector: 'app-task',
+    templateUrl: './task.component.html',
+    styleUrls: ['./task.component.css']
 })
 export class TaskComponent implements OnInit {
+    @Input() task: Task;
 
-  constructor() { }
+    constructor(private taskService:TaskService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
