b4d4a07a80a76fc0f199e53a37b5cf7446795353
MarijkeM <marijkemeersman@gmail.com>
2018-01-10 12:12:47 +0100

    timer werkt (nog niet gekoppeld)

--- frontend/src/app/components/timer/timer.component.ts
+++ frontend/src/app/components/timer/timer.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs'
+import {Observable, Subscription} from 'rxjs'
 
 @Component({
     selector: 'app-timer',
@@ -8,7 +8,14 @@ import {Observable} from 'rxjs'
 })
 export class TimerComponent implements OnInit {
 
-    ticks = 0;
+    ticks: number = 0;
+    minutes: number = 0;
+    hours: number = 0;
+    seconds: number=0;
+
+    sub: Subscription;
+
+    timer = Observable.timer(0, 1000);
 
     constructor() {
     }
@@ -17,8 +24,33 @@ export class TimerComponent implements OnInit {
     }
 
     onClickStart() {
-        let timer = Observable.timer(0, 1000);
-        timer.subscribe(t => this.ticks = t);
+
+        this.sub = this.timer.subscribe(
+            t => {
+                this.ticks = t;
+
+                this.seconds = this.getSeconds(this.ticks);
+                this.minutes = this.getMinutes(this.ticks);
+                this.hours = this.getHours(this.ticks);
+            }
+        );
+
+    }
+
+    private getSeconds(ticks: number) {
+        return this.pad(ticks % 60);
+    }
+
+    private getMinutes(ticks: number) {
+        return this.pad((Math.floor(ticks / 60)) % 60);
+    }
+
+    private getHours(ticks: number) {
+        return this.pad(Math.floor((ticks / 60) / 60));
+    }
+
+    private pad(digit: any) {
+        return digit <= 9 ? '0' + digit : digit;
     }
 
     onClickStop() {
